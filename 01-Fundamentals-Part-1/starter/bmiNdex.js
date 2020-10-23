e19cecd8fbacb6598280c034c7552cf0962bed2e
Thisislearner <ak97plutonium@gmail.com>
2020-10-23 10:48:53 +0530

    bMi index
    Created the bmi index of John and Sam

---
+++ 01-Fundamentals-Part-1/starter/bmiNdex.js
@@ -0,0 +1,10 @@
+const johnWeight = 98;
+const johnheight = 88;
+
+let samWeight = 1.98;
+let samHeight = 1.76;
+
+const bimJohn = johnWeight / (johnheight ** 2);
+const bimSam = samWeight / (samHeight ** 2);
+
+console.log(bimJohn, bimSam);
