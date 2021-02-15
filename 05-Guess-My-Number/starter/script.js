bf6a88fb478e805ff9dca7c5757a9d2465ecdada
Thisislearner <ak97plutonium@gmail.com>
2021-02-15 14:25:41 +0530

    commiting all the changes made in the file during practice

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+//secret number created number using math random and 1 is added so that it starts from 1 and ends at 20;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+const check = document.querySelector('.check');
+
+//function for writing the text message;
+const message = function (inputMessage) {
+  return (document.querySelector('.message').textContent = inputMessage);
+};
+
+// function for checking the guessed number.
+const guessCheck = function () {
+  console.log('hello world');
+};
+
+//giving the check command for check function;
+check.addEventListener('click', guessCheck);
