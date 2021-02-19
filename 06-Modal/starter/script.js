3c4c2e25af6279dc176b4b27f48e0f31ab87eac7
Thisislearner <ak97plutonium@gmail.com>
2021-02-19 06:10:46 +0530

    made changes in the css and javascript file.

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,34 +1,9 @@
 'use strict';
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnOpenModal);
+const show_modal = document.querySelectorAll('.show-modal');
 
-//removing hidden class
-const openModal = function(){
-  console.log('button clicked');
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+const show_modalFunc = function () {
+  const hiddenClass = document.querySelector('.modal');
 };
 
-//adding the hidden class
-const closeModal = function(){
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-
-//the for loop to select the different buttons
-for(let i = 0; i<btnOpenModal.length; i++)
-btnOpenModal[i].addEventListener('click', openModal);
-
- btnCloseModal.addEventListener('click',closeModal);
- overlay.addEventListener('click', closeModal);
-
-//function for the Escape key.
-let escapekey = function(e){
-  if(e.key === 'Escape' && !modal.classList.contains('hidden'))
-  closeModal();
-}
-
-document.addEventListener('keydown', escapekey);
+for (let i = 0; i < show_modal.length; i++)
+  show_modal[i].addEventListener('click', show_modalFunc);
